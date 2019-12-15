ed3c43598e1c6171b2c5ae2e9b88187aaa48182a
prantick.ghosh <prantick02@gmail.com>
2019-12-14 21:24:57 -0500

    Updated test file

--- project-manager-client-app/src/app/app.component.spec.ts
+++ project-manager-client-app/src/app/app.component.spec.ts
@@ -1,35 +1,47 @@
 import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { Routes, RouterModule } from "@angular/router";
+import { AddUserComponent } from './add-user/add-user.component';
+import { FormsModule } from '@angular/forms';
+import { FilterUserPipe } from './pipes/filter-user.pipe';
+import { AddProjectComponent } from './add-project/add-project.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { ViewTaskComponent } from './view-task/view-task.component';
+import { BsDatepickerModule } from 'ngx-bootstrap';
+import { Ng5SliderModule } from 'ng5-slider';
+import { ToastrModule } from 'ngx-toastr';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
+    const routes: Routes = [
+      { path: '', redirectTo: 'user', pathMatch: 'full' },
+      { path: 'user', component: AddUserComponent }
+    ];
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        FormsModule,
+        BsDatepickerModule.forRoot(),
+        ToastrModule.forRoot({
+          timeOut: 3000,
+          positionClass: 'toast-bottom-right',
+          preventDuplicates: true
+        }),
+        Ng5SliderModule,
+        RouterModule.forRoot(routes, { useHash: true })
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        AddUserComponent,
+        FilterUserPipe,
+        AddProjectComponent,
+        AddTaskComponent,
+        ViewTaskComponent
       ],
     }).compileComponents();
   }));
-
-  it('should create the app', () => {
+  it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'project-manager'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('project-manager');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to project-manager!');
-  });
+  }));
 });
