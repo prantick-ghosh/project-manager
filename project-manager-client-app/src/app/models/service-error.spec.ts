ea4f5c9a41ef3de2b9eee51c9626a52b0e650c2a
prantick-ghosh <prantick02@gmail.com>
2019-12-15 08:53:02 +0530

    Removed unused test files

--- project-manager-client-app/src/app/models/service-error.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ServiceError } from './service-error';
-
-describe('ServiceError', () => {
-  it('should create an instance', () => {
-    expect(new ServiceError()).toBeTruthy();
-  });
-});
